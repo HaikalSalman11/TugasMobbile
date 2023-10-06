4d869cdce274c86b972691907a506e711ef1438e
HaikalSalman11 <haikalsalman02@gmail.com>
2023-10-06 10:15:15 +0700

    first commit

---
+++ string.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Membuat 3 variabel string
+  String string1 = "Haikal";
+  String string2 = " ";
+  String string3 = "Salman";
+
+  // Menggabungkan ketiga string
+  String hasilGabungan = string1 + string2 + string3;
+
+  // Menampilkan hasilnya di layar
+  print(hasilGabungan);
+}
